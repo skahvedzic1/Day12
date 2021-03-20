edb76683f473909c62df57ea9d7f22c2ac31360b
skahvedzic1 <49349246+skahvedzic1@users.noreply.github.com>
2021-03-20 23:08:58 +0100

    ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+function myRecursion(arr, n) {
+    if (n <= 0) {
+        return 0;
+    } else {
+        return myRecursion(arr, n - 1) + arr[n - 1];
+    }
+}
+
+console.log(myRecursion([1, 2, 3, 4], 3));
+console.log(myRecursion([1, 2, 3, 4, 5, 6], 6));
+module.exports = myRecursion;
